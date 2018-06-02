3c184ec0162ecf01e55b9ace091b93ef18303f72
Gerolf Vent <gerolf.vent@gmx.de>
2018-06-02 19:29:34 +0200

    Optionen-Overlay hinzugefügt, Layer-Konfiguration ausgelagert

--- frontend-web/map.js
+++ frontend-web/map.js
@@ -1,24 +1,11 @@
+var mapLayers = null;
+var mapOverlay = document.getElementById('map-overlay');
+
 var map = new ol.Map({
 	target: 'map',
 	layers: [
 		new ol.layer.Tile({
 			source: new ol.source.OSM()
-		}),
-		new ol.layer.Heatmap({
-			source: new ol.source.Vector({
-			format: new ol.format.GeoJSON(),
-			loader: function(extent, resolution, projection) {
-				var xhr = new XMLHttpRequest();
-				xhr.open('GET', null); // TODO: insert request URL
-				xhr.onload = function() {
-					if (xhr.status == 200) {
-						map.addFeatures(map.getFormat().readFeatures(xhr.responseText));
-					}
-				}
-				xhr.send();
-			},
-			strategy: ol.loadingstrategy.bbox
-		 })
 		})
 	],
 	view: new ol.View({
@@ -27,3 +14,38 @@ var map = new ol.Map({
 	})
 });
 
+function downloadMapLayers() {
+	var xhr = new XMLHttpRequest();
+	xhr.addEventListener("readystatechange", function() {
+		if (xhr.status == 200) {
+			if(mapLayers == null) {
+				mapLayers = JSON.parse(xhr.responseText).layers;
+				addMapLayersToMap();
+			}
+		}
+	});
+	xhr.open("GET", "map-layers.json");
+	xhr.send();
+}
+
+function addMapLayersToMap() {
+	for (var i = 0; i < mapLayers.length; i++) {
+		var vectorSource = new ol.source.Vector({
+			format: new ol.format.GeoJSON(),
+			url: mapLayers[i].url,
+			strategy: ol.loadingstrategy.bbox
+		});
+
+		var vectorLayer = new ol.layer.Heatmap({
+			source: vectorSource,
+			gradient: (mapLayers[i].envpriority > 0 ? ['#F1F8E9', '#8BC34A'] : ['#FFEBEE', '#F44336']),
+			opacity: Math.abs(mapLayers[i].envpriority) / 100
+		});
+
+		mapLayers.olLayer = vectorLayer;
+
+		map.addLayer(vectorLayer);
+
+		mapOverlay.innerHTML += "<label class=\"mdl-switch mdl-js-switch mdl-js-ripple-effect\" for=\"" + mapLayers[i].id + "\"><input type=\"checkbox\" id=\"" + mapLayers[i].id + "\" class=\"mdl-switch__input\" checked><span class=\"mdl-switch__label\">" + mapLayers[i].name + "</span></label>";
+	}
+}
