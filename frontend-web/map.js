ab2e408274f2d7d8b6955fa072ffbc3cda82175f
Gerolf Vent <gerolf.vent@gmx.de>
2018-06-03 10:48:25 +0200

    Beste Orte (Näherung) hinzugefügt, Style verbessert

--- frontend-web/map.js
+++ frontend-web/map.js
@@ -1,12 +1,37 @@
 var mapLayers = null;
 var mapOverlayOptions = document.getElementById('map-overlay-options');
 
+/*var specialHeatmap = new ol.layer.Heatmap({
+			source: new ol.source.Vector({
+				format: new ol.format.GeoJSON(),
+				url: "best-places-0.01.json",
+				strategy: ol.loadingstrategy.bbox
+			}),
+			radius: 20
+		});
+
+specialHeatmap.getSource().on('addfeature', function(event) {
+	var envpriority = event.feature.get('envpriority');
+	console.log(envpriority);
+	event.feature.set('weight', envpriority / 100);
+});*/
+
+var best5Places = new ol.layer.Vector({
+	source: new ol.source.Vector({
+		format: new ol.format.GeoJSON(),
+		url: "best-5-places.json",
+		strategy: ol.loadingstrategy.bbox
+	}),
+	radius: 20
+});
+
 var map = new ol.Map({
 	target: 'map',
 	layers: [
 		new ol.layer.Tile({
 			source: new ol.source.OSM()
-		})
+		}),
+		best5Places
 	],
 	view: new ol.View({
 		center: ol.proj.fromLonLat([12.110148, 54.123435]),
@@ -40,7 +65,8 @@ function addMapLayersToMap() {
 			source: vectorSource,
 			gradient: (mapLayers[i].envpriority > 0 ? ['#F1F8E9', '#8BC34A'] : ['#FFEBEE', '#F44336']),
 			opacity: Math.abs(mapLayers[i].envpriority) / 100,
-			radius: 20 * Math.abs(mapLayers[i].envpriority) / 100
+			radius: 20 * Math.abs(mapLayers[i].envpriority) / 100,
+			shadow: 0
 		});
 
 		mapLayers[i].olLayer = vectorLayer;
