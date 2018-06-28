0ae97da9d1d0f4de30120efa1104102dc39a9ab3
asqiir <asqiir@blaupost.de>
2018-06-28 08:22:12 +0200

    verfying test

--- django_project_peacefinder/mapoutput/static/mapoutput/client.js
+++ django_project_peacefinder/mapoutput/static/mapoutput/client.js
@@ -1,6 +1,10 @@
 window.onload = function() {
-	const out = document.createElement('p');
-	out.innerHTML = "and js did too!";
-	const body = document.getElementsByTagName('body')[0];
-	body.appendChild(out);
+
+var map = new ol.Map({target: 'map'});
+
+map.setView(new ol.View({
+    center: [0, 0],
+    zoom: 2
+  }));
+
 };
