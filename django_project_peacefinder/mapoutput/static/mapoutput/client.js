d0c52b67b0b34c6c9d96cacc0b20089fc6e20995
asqiir <asqiir@blaupost.de>
2018-06-30 12:07:33 +0200

    rostock map shown. openlayers v5.0.0

--- django_project_peacefinder/mapoutput/static/mapoutput/client.js
+++ django_project_peacefinder/mapoutput/static/mapoutput/client.js
@@ -1,27 +1,22 @@
-//window.onload = function() {
 
-//var map = new ol.Map({target: 'map'});
-
-//map.setView(new ol.View({
-//    center: [0, 0],
-//    zoom: 2
-//  }));
-
-//};
 
 window.onload = function() {
 
-var map = new ol.Map({
-        target: 'map',
-        layers: [
-          new ol.layer.Tile({
-            source: new ol.source.OSM()
-          })
-        ],
-        view: new ol.View({
-          center: ol.proj.fromLonLat([12.110148, 54.123435]),
-          zoom: 12
+
+
+	const map = new ol.Map({
+    target: 'map',
+    layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
         })
-      });
+    ],
+    view: new ol.View({
+        center: ol.proj.fromLonLat([12.110148, 54.123435]),
+          zoom: 13
+        })
+    }
+);
+
 
 };
