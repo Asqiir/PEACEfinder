39c67bc55cd0d2ffcb1aac26666a8404fc16b9ac
asqiir <asqiir@blaupost.de>
2018-07-01 10:44:00 +0200

    project architecture comments written down

--- django_project_peacefinder/mapoutput/models.py
+++ django_project_peacefinder/mapoutput/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+# Models:
+
+# 1. points (from which file, latitude, longtitude)
+# 2. json file metadata (name, url, reloading frequency, last reload, green/red, radius(=importance))
